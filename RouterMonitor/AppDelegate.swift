65155769a69493ecc3116b4a0abdbd16ac3dc897
Julian Lima <JulianLima@macbook-pro-de-julian.local>
2016-08-20 14:43:31 -0300

    Initial Commit

---
+++ RouterMonitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RouterMonitor
+//
+//  Created by Julian Lima on 20/8/16.
+//  Copyright © 2016 Julian Lima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RouterMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RouterMonitor
+//
+//  Created by Julian Lima on 20/8/16.
+//  Copyright © 2016 Julian Lima. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
